6b515dadf1de2a98d4cda685b4b202acef10de9a
MaxTihon <maksim9542@gmail.com>
2021-12-22 14:12:24 +0300

    Initial commit

---
+++ src/main/java/com/maxtihon/tacocloud/model/Ingredient.java
@@ -0,0 +1,12 @@
+package com.maxtihon.tacocloud.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+}
---
+++ src/main/java/com/maxtihon/tacocloud/model/Type.java
@@ -0,0 +1,9 @@
+package com.maxtihon.tacocloud.model;
+
+public enum Type {
+    WRAP,
+    PROTEIN,
+    VEGGIES,
+    CHEESE,
+    SAUCE
+}
